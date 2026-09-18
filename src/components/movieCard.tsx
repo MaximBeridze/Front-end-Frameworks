6a480d0f120caeed2cd261e70a2cbee4b90c9698
maxim beridze <maxim.beridze@epita.fr>
2026-09-18 04:47:30 +0200

    Favourites button created faovourites functionality

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import type {Movie} from "../types"
 import { getGenreNames } from '../data/genres'
 import { TMDB_BACKDROP_BASE, TMDB_IMAGE_BASE } from '../data/sampleMovies'
+import { useState } from 'react'
 
 
 const MovieCard = ({ movie }: { movie: Movie }) => {  
   const genreNames = getGenreNames(movie.genre_ids)
   const imagePath = TMDB_IMAGE_BASE + movie.poster_path
   const backdropPath = TMDB_BACKDROP_BASE + movie.backdrop_path
+  const [isFav, setIsFav] = useState(false)
   return(
   <article className="movie-card" tabIndex={0} aria-label="title here ...">
       <div className="poster-wrapper">
@@ -19,7 +21,7 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
               <svg viewBox="0 0 24 24"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/></svg>
               {movie.vote_average?.toFixed(1) ?? "--"}
             </span>
-            <button className="favorite-btn " title="Add to Watchlist">
+            <button className={isFav ? "favorite-btn is-favorite" : "favorite-btn"} aria-label={isFav ? "Remove from favourites" : "Add to favourites"} onClick= {() => setIsFav(previous => !previous)}>
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l8.78-8.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
               </svg>
